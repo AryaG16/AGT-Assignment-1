8b939848cc2cd20f4250ea11ed7dbeba2cb9b3a3
Aryan Bhavsar <aryanbhavsar161202@gmail.com>
2025-01-19 15:38:01 +0530

    Getting Minor sub-matrix of Laplacian

---
+++ gaussianE.py
@@ -0,0 +1,6 @@
+
+#YET TO BE DONE
+def gaussian_elimination(M):
+    n=len(M)
+    for i in range(0,n-1):
+        pass
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from generateG import GraphGenerator, DrawGraph
 from laplacianM import GenerateLaplacianMatrix
 from determinantML import DetOfTransformedMatrix
+from gaussianE import gaussian_elimination
 
+# Generating Random Graph of 11 nodes
 nodes = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]
 # nodes = [65, 66, 67]
 G = GraphGenerator(nodes)
 DrawGraph(G)
 
+# Making the Laplacian Matrix
 L = GenerateLaplacianMatrix(G)
 print("Laplacian Matrix")
 print(L)
 
-# LEFT TO DO
-# Transform The minor of Laplacian matrix to Augmented form(upper triangular) HERE 
-ML = L  # for now, for testing below function
+# Getting Minor Matrix of Laplacian
+n=len(L)
+print(" Order of Laplacian's Matrix: ", L.shape)
+ML=L[:n-1,:n-1]
+print(" Order of the Minor Matrix of Laplacian's Matrix: ", ML.shape)
+print("Minor Matrix of Laplacian's")
+print(ML)
 
-# Det of Transformed Matrix
-det_of_L_minor = DetOfTransformedMatrix(ML)
-print("No. of Spanning Tree for this graph is: ", det_of_L_minor)
+# Getting Upper Triangular Matrix using gaussian elimination
+UTM = gaussian_elimination(ML)  
+
+# Det of Upper Triangular Matrix
+# det_of_L_minor = DetOfTransformedMatrix(UTM)
+# print("No. of Spanning Tree for this graph is: ", det_of_L_minor)
